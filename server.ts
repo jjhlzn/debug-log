36f7e1a02f3072702384ccc4368e91f12abf0a7a
jjhlzn <jinjunhang@hotmail.com>
2016-12-27 10:04:28 +0800

    update

--- server.ts
+++ server.ts
@@ -1,21 +1,11 @@
-'use strict';
-
-/*
- * nodejs-express-mongoose-demo
- * Copyright(c) 2013 Madhusudhan Srinivasa <madhums8@gmail.com>
- * MIT Licensed
- */
-
-/**
- * Module dependencies
- */
-
 require('dotenv').config();
 
 var fs = require('fs');
 const join = require('path').join;
 const express = require('express');
 var mongoose = require('mongoose');
+var mongodb = require('mongodb');
+var socketio = require("socket.io");
 const config = require('./config');
 const morgan = require('morgan');
 
@@ -26,28 +16,49 @@ const app = express();
 /**
  * Expose
  */
-
 module.exports = app;
 
 app.use(morgan('dev'));
 
 // Bootstrap models 
-
 fs.readdirSync(models)
   .filter(file => ~file.search(/^[^\.].*\.js$/))
   .forEach(file => require(join(models, file))); 
 
 // Bootstrap routes
 require('./config/routes')(app);
+//var io = socketio.listen(app);
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 connect()
   .on('error', console.log)
   .on('disconnected', connect)
   .once('open', listen);
 
+mongodb.MongoClient.connect (config.db, function (err, db) {
+  db.collection('requests_capped', function(err, collection) {
+    // open socket
+    io.sockets.on("connection", function (socket) {
+      // open a tailable cursor
+      console.log("== open tailable cursor");
+      collection.find({}, {tailable:true, awaitdata:true, numberOfRetries:-1})
+        .sort({ $natural: 1 })
+        .each(function(err, doc) {
+          console.log(doc);
+          // send message to client
+          if (doc.type == "message") {
+            socket.emit("message",doc);
+        }
+      });
+    });
+  });
+
+});
+
 function listen () {
   if (app.get('env') === 'test') return;
-  app.listen(port);
+  server.listen(port);
   console.log('Express app started on port ' + port);
 }
 
